26a0eaaa6f3670f5c891407cd05273b072cb6846
changamanda <amanda.chang10@gmail.com>
2015-01-30 11:33:13 -0500

    Update test to check for randomness

--- spec/dice_roll_spec.rb
+++ spec/dice_roll_spec.rb
@@ -14,4 +14,13 @@ describe "#roll" do
     expect(roll).to be < 7
   end
 
+  it "should return a random value" do
+  	rolls = []
+  	100.times do
+  		rolls << roll
+  	end
+  	expect(rolls).to include(1, 2, 3, 4, 5, 6)
+  	expect(rolls).not_to include(0, 7)
+  end
+
 end
