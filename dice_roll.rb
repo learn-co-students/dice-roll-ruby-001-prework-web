698d4850c1425970d84d849ddfd6a02ec6e7395b
flats <dave@robador.com>
2015-10-05 17:35:34 -0400

    first commit - range and array solutions

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,5 +3,10 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
+  # simple solution is just:
+  # rand(1..6)
+  # array solution is:
+  die = [1, 2, 3, 4, 5, 6]
+  die[rand(die.length)]
   # code goes here
 end
