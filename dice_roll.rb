b187cb3bc600f50ce272eb94ff3131aac1c88e11
josias2121 <josias.vidrio.simontech.2016@gmail.com>
2015-10-30 16:37:38 +0000

    finished dice roll

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,5 +3,8 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  # code goes here
+  roll1 = rand(6)
+  roll2 = rand(6)
+  "the first dice will be #{roll1} and the second dice will be #{roll2}"
 end
+puts roll
