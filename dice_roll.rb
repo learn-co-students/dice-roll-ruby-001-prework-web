e8c906ba54c7ce836e66894271ac1f269c527321
dorothym <dorothy.v.moore@gmail.com>
2015-11-18 10:09:32 -0500

    generate random number from 1 to 6 using two methods

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,5 +3,22 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  return rand(1..6)
+  my_roll = rand(1..6)
+  #puts "roll: #{my_roll}"
+  return my_roll
 end
+
+# the following solution uses an array
+
+=begin
+def roll_array
+  a = (1..6).to_a
+  #puts a
+  random_index = rand(1..6)
+  puts "roll_array: #{a[random_index]}"
+  return a[random_index]
+end
+=end
+
+#roll()
+#roll_array()
