67d16cc67fc69b8b40e11eced3fd5ad7c999986c
lgyure <lukegyure@gmail.com>
2015-12-18 00:10:29 -0500

    define #roll method using range and array

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,5 +3,10 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  # code goes here
+  rand(1..6)
+end
+
+def roll
+  number_array = [1, 2, 3, 4, 5, 6]
+  number_array.sample
 end
