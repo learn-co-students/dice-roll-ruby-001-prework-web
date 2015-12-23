20df05c66e8ce80b2ab3a4ee972f25f9e4049eee
jonalexander <jonathandalexander@gmail.com>
2015-12-23 16:08:22 -0500

    Commit Dice Roll Prework

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,5 +3,21 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  # code goes here
+
+  # Generates random value between 1 and 6
+  roll_value = rand(1..6) 
+
+  # Returns roll value
+  roll_value
+
+  # Grabs a random value from the array of dice values
+  dice_values = [1, 2, 3, 4, 5, 6]
+  
+  #Defines another roll value based of the array
+  roll_value_2 = dice_values.sample
+
+  # Returns a second roll value using the array
+  roll_value_2
+
 end
+
