7a1291ec98a5fac0455f702d497d43b03078bcee
WilliamMena <williammena1991@gmail.com>
2016-01-07 01:27:37 -0500

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -4,4 +4,23 @@
 
 def roll
   # code goes here
+  return 1 + rand(6)
 end
+
+#2.2.3 :007 > def roll
+#2.2.3 :008?>   a = [1..6]
+#2.2.3 :009?>   return a.sample
+#2.2.3 :010?>   end
+# => :roll 
+#2.2.3 :011 > roll
+# => 1..6 
+
+#2.2.3 :012 > def roll
+#2.2.3 :013?>   a = [1, 2, 3, 4, 5, 6]
+#2.2.3 :014?>   return a.sample
+#2.2.3 :015?>   end
+# => :roll 
+#2.2.3 :016 > roll
+# => 4 
+#2.2.3 :017 > roll
+# => 1
