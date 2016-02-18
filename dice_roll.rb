b4ae1c373cd4c3126fe16e07c87484b9ce793d42
Tal <talgoldfus@gmail.com>
2016-02-18 06:53:17 +0000

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,5 +3,10 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
+  rand(1..6)
+  cube = [1,2,3,4,5,6]
+  cube.sample
+  roll= cube[rand(cube.length)]
+  roll
   # code goes here
 end
