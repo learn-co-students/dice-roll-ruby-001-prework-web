fe6724b184d253fdb840dc38c8c56c9278ad623a
snatow <natow.sarah@gmail.com>
2016-02-23 17:02:43 -0500

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,5 +3,6 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  # code goes here
+  choices = [1,2,3,4,5,6]
+  number = choices[rand(choices.length)]# code goes here
 end
