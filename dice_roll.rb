2c00fa70ac0e8f076a33a427418ab55f26333447
Steven Kwok <mrfishball@gmail.com>
2016-05-19 16:13:48 -0400

    Rolled the dice

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,5 +3,12 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  # code goes here
+  numArray = [1,2,3,4,5,6]
+  numArray.sample
 end
+
+# or
+
+#def roll
+#  rand(1...7)
+#end
