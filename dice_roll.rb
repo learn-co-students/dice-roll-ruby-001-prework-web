753cbfa134007fd56d068a1b6fdf730df0af46fd
Satu Barnhill <satu.barnhill@gmail.com>
2016-06-25 18:46:39 +0000

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,6 +2,16 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
+#1st method
+=begin
 def roll
-  # code goes here
+  result = rand(1..6)
+  result
+end
+=end
+#2nd method
+def roll
+  die_array = [1,2,3,4,5,6]
+  random_index = rand(0...6)
+  return die_array[random_index]
 end
