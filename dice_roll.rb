c195fb95eb4c34cf7a3951951a6b889702e40d37
smcarrera <stephanie.carrera@gmail.com>
2016-07-06 21:19:08 -0400

    dice roll, first try with range (commented out), second try with array

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,6 +2,17 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
+# first example using a range
+=begin
 def roll
-  # code goes here
+  dice_face = rand(1..6)
+end
+=end
+#
+
+# second example using an array
+def roll
+  dice_array = [1,2,3,4,5,6]
+  index = rand(dice_array.length)
+  dice_array.at(index)
 end
