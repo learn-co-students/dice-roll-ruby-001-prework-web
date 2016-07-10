a9de752501a5d9b43876399349d89497baec38e8
OliviaGunton <olivia.gunton@gmail.com>
2016-07-09 20:33:54 -0400

    Implement dice roll with rand(range) and with array

--- dice_roll.rb
+++ dice_roll.rb
@@ -1,7 +1,15 @@
 # Create method `roll` that returns a random number between 1 and 6
 
-# Feel free to google "how to generate a random number in ruby"
+#require 'pry'
 
 def roll
-  # code goes here
+  rand(1..6)
 end
+
+def roll_with_array
+  faces = [1, 2, 3, 4, 5, 6]
+  i = rand(0..5)
+  faces[i]
+end
+
+#binding.pry
