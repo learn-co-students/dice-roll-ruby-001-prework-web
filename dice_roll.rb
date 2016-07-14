df24cb4b3baf3d6f7113e57aaa823a2e6d51f72b
Steffi Balarezo <balarezos@gmail.com>
2016-07-14 17:57:07 +0000

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,5 +3,6 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  # code goes here
+  rand(6) #=> gives a random number between 0 and 6 inclusively
+  rand(1..6) #=> gives a random number between 1 and 6 inclusively 
 end
