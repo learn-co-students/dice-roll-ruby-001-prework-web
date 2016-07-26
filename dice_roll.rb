5e039d77121272e3e60ad7023a44fcb1d3e56c0c
Brie Coyle <brie.coyle@gmail.com>
2016-07-26 21:37:03 +0000

    Done.

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,6 +2,11 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
+#def roll
+#  return 1 + rand(6)
+#end
+
 def roll
-  # code goes here
+  dice_sides = [1, 2, 3, 4, 5, 6]
+  dice_sides.sample
 end
